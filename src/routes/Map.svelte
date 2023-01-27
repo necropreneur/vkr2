46fe49055d094f03580d532c50cdf4c75f21da71
Barni-Pack <fiji8800@gmail.com>
2023-01-27 04:42:34 +0300

    leaflet-geoman was setup

--- src/routes/Map.svelte
+++ src/routes/Map.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import * as L from "leaflet";
   import { onMount } from "svelte";
+  import "@geoman-io/leaflet-geoman-free";
+  import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
   import "leaflet/dist/leaflet.css";
   const bg = new URL("$lib/floors/2floor.png", import.meta.url).href;
 
@@ -16,41 +18,35 @@
       crs: L.CRS.Simple,
       // maxZoom: 2,
       minZoom: -10,
-      maxZoom: 0,
+      maxZoom: 3,
       zoomSnap: 0,
       // zoomSnap: 1,
-      // zoomDelta: 0.5,
+      zoomDelta: 0.3,
       maxBounds: bounds,
       attributionControl: false,
     });
-
-    
-    // map.setMinZoom(currentZoom);
-    
+    map.pm.addControls({
+      position: 'topleft',
+      // snappingOption: true,
+      // drawCircle: false,
+    })
+    map.pm.setGlobalOptions({ snappable: false }); 
 
     var image = L.imageOverlay(bg, bounds).addTo(map);
-    console.log(bg);
     map.fitBounds(bounds);
 
-    // var boundsImg = L.latLngBounds([
-    //   [0, 0],
-    //   [2000, 2000],
-    // ]);
-
-    var wantedZoom = map.getBoundsZoom(bounds, true);
-    var center = bounds.getCenter();
-    // map.setZoom(-1);
-    // map.setView([0, 0]);
     map.fitBounds(bounds);
 
-    var currentZoom = map.getZoom()
-    map.setMinZoom(currentZoom)
-    console.log(currentZoom)
+    var currentZoom = map.getZoom();
+    // console.log(currentZoom - 0.3)
+    // map.setMinZoom(currentZoom - 0.3)
+    // map.setZoom(currentZoom - 0.3)
+
     // const sol = L.latLng([145, 175]);
     // const marker = L.marker(sol).addTo(map);
   });
 </script>
 
-<div id="map" class="w-[600px] h-[300px]" />
+<div id="map" class="w-[900px] h-[500px] !bg-white" />
 
 <!-- <img src={bg} alt=""> -->
